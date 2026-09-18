472ae3109ffe216a1e6984afb46305e46e18b333
yanas <yanas@yanas.cz>
2026-09-18 17:50:56 +0200

    Fix precision of sub-sat fees in Mempool Status widgets

--- Widgets/Sources/Extensions/Text.swift
+++ Widgets/Sources/Extensions/Text.swift
@@ -3,7 +3,7 @@ import SwiftUI
 extension Text {
 
     init(mempoolFee value: Double) {
-        let precision = abs(value) < 1 ? 1 : 0
+        let precision = abs(value) < 0.95 ? 1 : 0
         self.init(value, format: .number.precision(.fractionLength(precision)))
     }
 
